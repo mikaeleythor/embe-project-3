edbd57b7674c9f93c60eb17f56f93035e876eb4a
mikaeleythor <eythor108@gmail.com>
2024-10-03 21:34:54 +0000

    Set the motor driver to use timer 2
    timer 2 is 8 bits and is preferred for PWM according to docs

--- code/src/motor_driver.cpp
+++ code/src/motor_driver.cpp
@@ -1,20 +1,16 @@
 #include <analog_out.hpp>
 #include <motor_driver.hpp>
 
+#define PWM_TIMER_NUM 2
+
 Motor_driver::Motor_driver(int pin_number) : pwm(Analog_out(pin_number)) {}
 
-void Motor_driver::init(int period_ms) {
-	pwm.init(period_ms);
+void Motor_driver::init(int period_ms) { pwm.init(period_ms, PWM_TIMER_NUM); }
+
+void Motor_driver::set_duty_cycle(int duty_cycle, int timer_num) {
+  pwm.set(duty_cycle, timer_num);
 }
 
-void Motor_driver::set_duty_cycle(int duty_cycle,int timer_num) {
-  pwm.set(duty_cycle,timer_num);
-}
+void Motor_driver::pwm_hi() { pwm.pin.set_hi(); }
 
-void Motor_driver::pwm_hi() {
-  pwm.pin.set_hi();
-}
-
-void Motor_driver::pwm_lo() {
-  pwm.pin.set_lo();
-}
+void Motor_driver::pwm_lo() { pwm.pin.set_lo(); }
