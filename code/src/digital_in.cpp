57ee974b04a26c76b8a66feb6f511b17abab80c8
mikaeleythor <eythor108@gmail.com>
2024-10-06 00:32:18 +0000

    breytti portib í portd

--- code/src/digital_in.cpp
+++ code/src/digital_in.cpp
@@ -4,15 +4,15 @@
 Digital_in::Digital_in(int pin_number) { pin_mask = (1 << pin_number); }
 
 void Digital_in::init() {
-  DDRB &= pin_mask; // Data direction register set to input
+  DDRD &= pin_mask; // Data direction register set to input
 }
 
 bool Digital_in::is_hi() {
-  bool state = (PINB & pin_mask); // Read state
+  bool state = (PIND & pin_mask); // Read state
   return state == 1;
 }
 
 bool Digital_in::is_lo() {
-  bool state = (PINB & pin_mask); // Read state
+  bool state = (PIND & pin_mask); // Read state
   return state == 0;
 }
